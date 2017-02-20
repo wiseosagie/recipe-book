402f03ed6c589107007cd8967cd7f644aff3e43e
User <user@Users-MacBook-Air.local>
2017-02-19 19:14:10 -0600

    first commit

---
+++ server/publish.js
@@ -0,0 +1,9 @@
+Meteor.publish('recipes', function(){
+  return Recipes.find({author: this.userId});
+});
+
+Meteor.publish('SingleRecipe', function(id){
+  check(id, String);
+
+  return Recipes.find({_id: id});
+});
